6430325d129a31b6439e3d5947b7d4dbebfbbe67
JasonTrapp <jason_t_28@protonmail.ch>
2017-06-07 01:51:11 +1000

    Added Javadocs

--- prac04-collections/src/question/HashBag.java
+++ prac04-collections/src/question/HashBag.java
@@ -5,20 +5,41 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Implementation of the bag data structure. Implements the removal, addition
+ * size, quantity and converts to a Set.
+ * @author Jason Trapp
+ *
+ * @param <T>
+ */
 public class HashBag<T> extends Bag<T> {
+	//Field that stores the values in the bag
 	private Map<T, Integer> bagContent;
 	
+	/**
+	 * Contructor that instantiates the bag.
+	 */
 	public HashBag(){
 		bagContent = new HashMap<T, Integer>();
 	}
 	
+	/**
+	 * 
+	 * @return 
+	 */
 	@Override
 	public int quantity(T element) {
 		if(bagContent.containsKey(element))
 			return bagContent.get(element);
 		return 0;
 	}
-
+	
+	/**
+	 * Adds a new element to the  bag.
+	 * @throws BagException Throws exception when the quantity added is negative.
+	 * @param newValue The object being added
+	 * @param quantity The amount of new objects added
+	 */
 	@Override
 	public void add(T newValue, int quantity) throws BagException {
 		if(quantity < 0)
@@ -29,6 +50,12 @@ public class HashBag<T> extends Bag<T> {
 			bagContent.put(newValue, quantity);
 	}
 
+	/**
+	 * 
+	 * @throws BagEXception Throws exception when quantity removed is negative.
+	 * @param oldValue The object being removed
+	 * @param quantity The amount of objects being removed.
+	 */
 	@Override
 	public void remove(T oldValue, int quantity) throws BagException {
 		if(quantity < 0)
@@ -38,17 +65,29 @@ public class HashBag<T> extends Bag<T> {
 		else
 			bagContent.put(oldValue, quantity(oldValue)-quantity);
 	}
-
+	
+	/**
+	 * Converts hashmap to a set
+	 * @return A set of the hashmap
+	 */
 	@Override
 	public Set<T> toSet() {
 		return bagContent.keySet();
 	}
-
+	
+	/**
+	 * Function which allows iteration over data structure
+	 * @return Iterator object to iterate over data structure
+	 */
 	@Override
 	public Iterator<T> iterator() {
 		return HashBagIterator<T>(bagContent.entrySet().iterator());
 	}
 
+	/**
+	 * Gets the size of the bag with respect to amount of elements in it
+	 * @return Size of the bag
+	 */
 	@Override
 	public int size() {
 		int size = 0;
--- prac04-collections/src/question/HashBagIterator.java
+++ prac04-collections/src/question/HashBagIterator.java
@@ -12,16 +12,27 @@ public class HashBagIterator<T> implements Iterator<T>{
 	
 	private int counter;
 	
+	/**
+	 * Instantiates the Iterator object
+	 */
 	public HashBagIterator(Iterator<Entry<T, Integer>> iterator){
 		collectionIterator = iterator;
 		advanceEntry();
 	}
 	
+	/**
+	 * Checks if the iterator has another value.
+	 * @return return true if there is another value, false otherwise
+	 */
 	@Override
 	public boolean hasNext() {
 		return current != null;
 	}
 
+	/**
+	 * Method which gets the next items in iterator object
+	 * @return returns a generic type which is the next item
+	 */
 	@Override
 	public T next() {
 		if(current == null)
@@ -37,11 +48,20 @@ public class HashBagIterator<T> implements Iterator<T>{
 		return nextItem;
 	}
 	
+	/**
+	 * Removes next item but throws exception.
+	 */
 	@Override
 	public void remove(){
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * Advance the current entry to the next available entry
+	 * containing a positive number of items (as it is not meaningful to repeat
+	 * zero or fewer times). If we have reached the end of the underlying
+	 * collection, current entry will be null. The current counter is set to 1
+	 */
 	private void advanceEntry(){
 		do{
 			current = collectionIterator.hasNext() ? collectionIterator.next(): null;
