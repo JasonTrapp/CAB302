34ee1fabce1653ee181c38e540be6974599f6da7
JasonTrapp <jason_t_28@protonmail.ch>
2017-06-07 19:49:45 +1000

    Added Javadocs

--- prac06-tdd/src/movieListQuestion/MovieList.java
+++ prac06-tdd/src/movieListQuestion/MovieList.java
@@ -3,13 +3,26 @@ package movieListQuestion;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * 
+ * @author Jason Trapp
+ *
+ */
 public class MovieList {
 	private Map<String, String> movies;
 	
+	/**
+	 * Constructor which initializes the treemap. Map accepts String input
+	 * for key and value
+	 */
 	public MovieList(){
 		 movies = new TreeMap<String, String>();
 	}
 
+	/**
+	 * Adds a new movie to the data set with a default rating of "No rating"
+	 * @param name The name of the movie being added
+	 */
 	public void addMovie(String name) throws MovieListException {
 		if(!movies.containsKey(name))
 			movies.put(name, "No rating");
@@ -17,13 +30,26 @@ public class MovieList {
 			throw new MovieListException("Cannot add a duplicate movie");
 	}
 
-	public Object getRating(String name) throws MovieListException {
+	/**
+	 * Function which retrieves the rating of a movie
+	 * @param name The name of the movie we want the rating of
+	 * @return The rating of the movie in question
+	 * @throws Exception thrown if movie does not exist
+	 */
+	public String getRating(String name) throws MovieListException {
 		if(movies.containsKey(name))
 			return movies.get(name);
 		else 
 			throw new MovieListException("Movie does not exist");
 	}
 
+	/**
+	 * Sets the rating of a movie
+	 * @param name The name of the movie being rated
+	 * @param i The rating applied to the movie
+	 * @throws Exception is thrown if movie does not exist, if the rating 
+	 * is less than or more than 5
+	 */
 	public void setRating(String name, int i) throws MovieListException {
 		if(!movies.containsKey(name))
 			throw new MovieListException("Movie does not exist");
@@ -39,14 +65,15 @@ public class MovieList {
 			movies.put(name, toEnter);
 		}
 	}
-
+	
+	/**
+	 * Gets all the movie names and separates them with a new line.
+	 * @return A list of the movies in the data structure in alphabetical order
+	 */
 	public String getList() {
 		String list = "";
 		for(Map.Entry<String, String> entry : movies.entrySet())
 			list += entry.getKey() + "\n";
 		return list;
-	}
-
-	
-	
+	}	
 }
