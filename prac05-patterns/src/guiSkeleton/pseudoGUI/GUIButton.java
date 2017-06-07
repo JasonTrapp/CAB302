a6b2deb2c5f3c8d065c6c83b8b7617e16d709c6a
JasonTrapp <jason_t_28@protonmail.ch>
2017-06-07 12:55:04 +1000

    Corrected the GUIButton and Display classes

--- prac05-patterns/src/guiSkeleton/pseudoGUI/GUIButton.java
+++ prac05-patterns/src/guiSkeleton/pseudoGUI/GUIButton.java
@@ -7,41 +7,61 @@ import guiSkeleton.guiEvents.GUIEvent;
 import guiSkeleton.guiEvents.GUIEventListener;
 import guiSkeleton.guiEvents.GUIEventSource;
 
+import java.util.ArrayList;
 import java.util.ListIterator;
-import java.util.Vector;
 
 
 /**
  * The Concrete Subject class for the Observer Pattern example 
- * @author hogan
+ * @author Jason Trapp
  *
  */
 public class GUIButton implements GUIEventSource {
 	private String name; 
 	private boolean clicked;
-	private Vector<GUIEventListener> guiEventListerners;
+	private ArrayList<GUIEventListener> guiEventListerners;
 	
+	/**
+	 * Constructor of the GUIButton class, set the name of the button
+	 * initialize the click status to false and initialize a new ArrayList
+	 * @param nm Name of the new button
+	 */
 	public GUIButton(String nm) {
 		this.name=nm;
 		this.clicked=false; 
-		this.guiEventListerners = XXXXXX;
+		this.guiEventListerners = new ArrayList<GUIEventListener>();
 	}
 	
+	/**
+	 * Function which gets the name of the button
+	 * @return The name of the button
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Change the name of the button.
+	 * @param name The new name of the button
+	 * @return The new name of the button
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Implementation of the doClick, when clicked triggers the GUI event
+	 */
 	@Override
 	public void doClick() {
-		XXXXXX;
-		XXXXXX;
-		XXXXXX;
+		clicked = true;
+		fireGUIEvent();
+		clicked = false;
 	}
 
+	/**
+	 * Implementation of the GUI execution event
+	 */
 	@Override
 	public void fireGUIEvent() {
 		ListIterator<GUIEventListener> iter = this.guiEventListerners.listIterator();
@@ -50,16 +70,26 @@ public class GUIButton implements GUIEventSource {
 		}
 	}
 
+	/**
+	 * Adds a listener to the eventListener
+	 */
 	@Override
 	public void addGUIEventListener(GUIEventListener gel) {
-		XXXXXX;
+		this.guiEventListerners.add(gel);
 	}
 
+	/**
+	 * Removes a listener from the eventListner
+	 */
 	@Override
 	public void removeGUIEventListener(GUIEventListener gel) {
-		XXXXXX;
+		this.guiEventListerners.remove(gel);
 	}
 	
+	/**
+	 * Gets the name of the button into a string
+	 * @return the name of the button with prepended name
+	 */
 	@Override
 	public String toString() { 
 		return "guiButton:" + this.name; 
--- prac05-patterns/src/guiSkeleton/pseudoGUI/GUIDisplay.java
+++ prac05-patterns/src/guiSkeleton/pseudoGUI/GUIDisplay.java
@@ -5,22 +5,38 @@ import guiSkeleton.guiEvents.GUIEventListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-
+/**
+ * Class which displays the actionPerformed to console
+ * @author Jason Trapp
+ *
+ */
 public class GUIDisplay implements GUIEventListener {
 	public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
 	private String regionName;
 	
+	/**
+	 * Constructor which sets the region name
+	 * @param rn The region name being added
+	 */
 	public GUIDisplay(String rn) {
 		this.regionName=rn;
 	}
 
+	/**
+	 * Prints the action to the console
+	 * @param ge Gets the type of action performed
+	 */
 	@Override
 	public void guiActionPerformed(GUIEvent ge) {
-		String str = this.regionName+ ":" + XXXXXX + ":" XXXXXX + 
+		String str = this.regionName+ ":" + ge.toString() + ":" + ge.getSource().toString()+ 
 		":" + this.now();
 		System.out.println(str);
 	}
 
+	/**
+	 * Returns the current date and time in a readable format
+	 * @return A printable String of the date and time
+	 */
 	private String now() {
 	  Calendar cal = Calendar.getInstance();
 	  SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
